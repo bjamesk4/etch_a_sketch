d8d1f9f33186cc1afe11c3d7e15f792b86e51ff1
bjamesk4 <bjamesk4@gmail.com>
2021-10-09 14:13:31 -0700

    added clear button and slider and minimal functionality for both

--- etch.js
+++ etch.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector('.container');
+const clearButton = document.querySelector('.clearButton');
 
 
 
@@ -12,11 +13,38 @@ document.querySelectorAll('.gridCell').forEach((gridCell) => {
     })
 });
 
+//on click of Clear Button clears all cells
+clearButton.addEventListener('click', function() {
+    clearGrid()
+});
+
+//slider functionality 
+const slider = document.getElementById('range');
+const sliderOut = document.getElementById('sliderValue');
+sliderOut.innerHTML = slider.value + 'x' + slider.value;
+
+//updates text values on slider change
+slider.oninput = function() {
+    sliderOut.innerHTML = this.value + 'x' + this.value;
+}
+
+//clear grid after slider release
+slider.onchange = function() {
+    clearGrid();
+}
+
 //create a group of square divs
 function createGrid() {
-    for(let i = 0; i < 256; i++) {
+    for (let i = 0; i < 256; i++) {
         const gridCell = document.createElement('div');
         gridCell.classList.add('gridCell');
         gridContainer.appendChild(gridCell);
     }
+}
+
+//clear all colored cells
+function clearGrid() {
+    document.querySelectorAll('.gridCell').forEach((gridCell) => {
+            gridCell.classList.remove('transitioned');
+    });
 }
